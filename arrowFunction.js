bef58715554435e43e05be0a93717ace01fc6308
yosf AL-otaibi <yousefalotaini21@gmail.com>
2024-11-04 20:18:00 +0300

    add commit

--- arrowFunction.js
+++ arrowFunction.js
@@ -1,13 +1,14 @@
+//task 1
 const greet = (name) => `Hello, ${name}!`;
 console.log(greet("yousef"));
-
+//task 2
 const numbers = [23, 34, 59, 576];
 let sum = 0;
 sum = numbers.reduce((total, numbers) => {
   return total + numbers;
 });
 console.log(sum);
-
+// task 3
 let squares = [];
 squares = numbers.map((number) => {
   return number ** 2;
