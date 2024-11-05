057f6f88c300dc42fd64d72705258da30d843c6f
yosf AL-otaibi <yousefalotaini21@gmail.com>
2024-11-05 15:07:14 +0300

    update

--- arrowFunction.js
+++ arrowFunction.js
@@ -2,6 +2,13 @@
 const greet = (name) => `Hello, ${name}!`;
 console.log(greet("yousef"));
 //task 2
+const performMath = (operation, firstNum, secondNum) => {
+  return operation(firstNum, secondNum);
+};
+const addition = (firstNum, secondNum) => firstNum + secondNum;
+
+console.log(performMath((firstNum, secondNum) => firstNum + secondNum, 58, 89));
+///
 const numbers = [23, 34, 59, 576];
 let sum = 0;
 sum = numbers.reduce((total, numbers) => {
